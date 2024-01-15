8c829dcd6ffad94ea663642f03a1a6fc5948913b
sawm2000 <sabreenmaflahi@outlook.com>
2024-01-15 15:07:18 +0000

    completed GET /api/articles

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,8 @@ const app = require("../app");
 const request = require("supertest");
 const seed = require("../db/seeds/seed");
 const testData = require("../db/data/test-data/");
-const endpoints = require("../endpoints.json")
+const endpoints = require("../endpoints.json");
+const sorted = require("jest-sorted");
 
 beforeEach(() => {
   return seed(testData);
@@ -44,7 +45,7 @@ describe("/api", () => {
         //   }
         // });
 
-        expect(response.body.endpoints).toEqual(endpoints)
+        expect(response.body.endpoints).toEqual(endpoints);
       });
   });
 });
@@ -64,7 +65,7 @@ describe("/api/articles/:article_id", () => {
           article_img_url:
             "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
         });
-        expect(typeof response.body.article.created_at).toBe("string")
+        expect(typeof response.body.article.created_at).toBe("string");
       });
   });
   test("GET 404: should return 404 status code an error message when given a valid but non-existent id", () => {
@@ -82,6 +83,48 @@ describe("/api/articles/:article_id", () => {
       .expect(400)
       .then((response) => {
         expect(response.body.message).toBe("Bad Request");
+      });
+  });
+});
+describe("/api/articles", () => {
+  test("GET 200: should return an array of article objects", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.articles.length).toBe(13);
+        response.body.articles.forEach((article) => {
+          expect(typeof article.article_id).toBe("number");
+          expect(typeof article.author).toBe("string");
+          expect(typeof article.title).toBe("string");
+          expect(typeof article.topic).toBe("string");
+          expect(typeof article.created_at).toBe("string");
+          expect(typeof article.votes).toBe("number");
+          expect(typeof article.article_img_url).toBe("string");
+          expect(typeof article.comment_count).toBe("string");
+        });
+      });
+  });
+
+  test("GET 200: should return an array of article objects - sorted by date in descending order.", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.articles).toBeSortedBy("created_at", {
+          descending: true,
+        });
+        expect(response.body.articles[0]).toMatchObject({
+          article_id: 3,
+          title: "Eight pug gifs that remind me of mitch",
+          author: "icellusedkars",
+          topic: "mitch",
+          created_at: "2020-11-03T09:12:00.000Z",
+          votes: 0,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          comment_count: "2",
+        });
       });
   });
 });
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { getTopics, getEndpoints } = require("./controllers/topics.controller");
 
-const { getArticleById } = require("./controllers/articles.controller");
+const { getArticleById, getArticles } = require("./controllers/articles.controller");
 
 const app = express();
 
@@ -10,6 +10,8 @@ app.get("/api/topics", getTopics);
 app.get("/api", getEndpoints);
 
 app.get("/api/articles/:article_id", getArticleById);
+
+app.get("/api/articles", getArticles);
 
 app.use((err, req, res, next) => {
   if (err.status && err.message) {
