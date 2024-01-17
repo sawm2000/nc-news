f57b2ea240965482ff28dbc89845aaf8336f10cb
sawm2000 <sabreenmaflahi@outlook.com>
2024-01-17 11:28:52 +0000

    refactor

--- db/connection.js
+++ db/connection.js
@@ -4,14 +4,15 @@ const ENV = process.env.NODE_ENV || "development";
 
 const config = {};
 
-if (ENV === "production") {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
-}
-
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL
+  config.max = 2;
+  console.log(process.env.DATABASE_URL)
+}
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
